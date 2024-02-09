3286ff868d2f3ff3cd5ff5d175551f93415795b6
Bahuro <bautistahuingojuanroimer@gmail.com>
2024-02-09 15:52:48 -0500

    "mas mejoras"

--- menuControl.xaml.cs
+++ menuControl.xaml.cs
@@ -1,27 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace ControlNavegación
 {
-    /// <summary>
-    /// Lógica de interacción para menuControl.xaml
-    /// </summary>
     public partial class menuControl : Window
     {
         public menuControl()
         {
             InitializeComponent();
         }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem menuItem = (MenuItem)sender;
+            string header = menuItem.Header?.ToString();
+
+            if (header != null)
+            {
+                switch (header)
+                {
+                    case "Inicio":
+                        contentGrid.Children.Clear();
+                        contentGrid.Children.Add(new TextBlock() { Text = "Estás en la página de Inicio." });
+                        break;
+                    case "Productos":
+                        contentGrid.Children.Clear();
+                        contentGrid.Children.Add(new TextBlock() { Text = "Aquí están los productos disponibles." });
+                        break;
+                    case "Carrito de Compras":
+                        contentGrid.Children.Clear();
+                        contentGrid.Children.Add(new TextBlock() { Text = "Tu carrito de compras está vacío." });
+                        break;
+                    case "Salir":
+                        
+                        break;
+                }
+            }
+        }
+
     }
 }
