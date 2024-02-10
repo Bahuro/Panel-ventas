56a73e8ab9d60838ed3563204648b5314046f324
Dayaacp <dcasaperaltap21_1@unc.edu.pe>
2024-02-09 19:11:32 -0500

    "categorias"

--- menuControl.xaml.cs
+++ menuControl.xaml.cs
@@ -72,8 +72,31 @@ namespace ControlNavegación
                                               "Kumquat", "Níspero", "Chirimoya", "Caqui", "Membrillo", "Zarzamora", "Pitahaya", "Nectarina", "Castaña",
                                               "Cereza de Barbados", "Naranja sanguina", "Melocotón", "Níspero japonés", "Tamarillo", "Feijoa", "Rambután", 
                                               "Tuna", "Grosella", "Ciruela pasa", "Yuzu", "Jabuticaba", "Noni" };
-               
-               
+                case "Carnes":
+
+                    return new List<string> { "Pollo", "Res", "Cerdo", "Pavo", "Cordero", "Pato", "Ternera", "Codorniz", "Conejo", "Liebre",
+                                              "Faisán", "Ciervo", "Búfalo", "Jabalí", "Carnero", "Cabra", "Avestruz", "Pichón", "Paloma", "Ganso", "Salmón", "Atún",
+                                              "Trucha", "Bacalao", "Lenguado", "Merluza", "Sardina", "Anchoa", "Bacalao", "Anguila", "Pejerrey", "Dorada", "Mero",
+                                              "Besugo", "Pez espada", "Tilapia", "Carpa", "Róbalo", "Calamar", "Pulpo", "Langosta", "Cangrejo", "Almeja", "Mejillón",
+                                              "Ostra", "Caracol", "Chorizo", "Salchicha", "Jamón" };
+                case "Lacteos":
+
+                    return new List<string> { "Leche", "Queso", "Yogur", "Mantequilla", "Nata", "Helado", "Postres lácteos", "Leche condensada", 
+                                              "Leche evaporada", "Leche en polvo", "Kéfir", "Crema agria", "Requesón", "Cuajada", "Queso crema", 
+                                              "Ricotta", "Mozzarella", "Gruyère", "Roquefort", "Brie", "Camembert", "Feta", "Provolone", "Gouda", "Emmental", "Manchego", 
+                                              "Parmesano", "Cheddar", "Gorgonzola", "Cottage cheese", "Mascarpone", "Crema de queso azul", 
+                                              "Leche de cabra", "Leche de oveja", "Leche de búfala", "Yogur griego", "Yogur de frutas", "Yogur natural", 
+                                              "Yogur desnatado", "Yogur sin lactosa", "Helado de vainilla", "Helado de chocolate", "Helado de fresa", 
+                                              "Helado de vainilla con trozos de galleta", "Helado de pistacho", "Helado de nueces", "Flan de vainilla" };
+                case "Bebidas":
+
+                    return new List<string> { "Coca-Cola", "Pepsi", "Sprite", "Fanta", "7Up", "Mirinda", "Dr Pepper", "Mountain Dew", "Schweppes", 
+                                              "Canada Dry", "Cerveza Corona", "Heineken", "Budweiser", "Guinness", "Stella Artois", "Modelo", "Victoria", 
+                                              "Tecate", "Dos Equis", "Red Bull", "Monster Energy", "Rockstar Energy", "Gatorade", "Powerade", "Arizona", 
+                                              "Snapple", "Nestea", "Lipton Ice Tea", "Tropicana", "Minute Maid", "Del Valle", "Fruitopia", "Ciel", 
+                                              "Bonafont", "Dasani", "San Pellegrino", "Perrier", "Evian", "Aquafina", "Volvic", "Topo Chico", "Jarritos", 
+                                              "Fresca", "Squirt", "A&W Root Beer", "Barq's", "Sunkist", "Canada Dry Ginger Ale" };
+
                 default:
                     return new List<string>(); // Retorna una lista vacía si no se encuentra la categoría
             }
