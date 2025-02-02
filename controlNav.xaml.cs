05d2ddc0e6a5a492028ac53b768e80cc3065d74a
Juan Roimer Bautista Huingo <bautistahuingojuanroimer@gmail.com>
2025-02-01 23:42:49 -0500

    "interfas mas intuitiva ahora podras agregar y eliminar productos y  añadir datos del comprador "

--- controlNav.xaml.cs
+++ controlNav.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace ControlNavegación
 {
@@ -21,100 +13,104 @@ namespace ControlNavegación
             InitializeComponent();
             MostrarPersonas();
             this.KeyDown += MainWindow_KeyDown;
-
         }
-        private void TabControl_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            
             if (InicioTab.IsSelected)
             {
-                
                 MostrarPersonas();
             }
             else if (ProductosTab.IsSelected)
             {
-               
                 CargarProductos();
             }
             else if (ContactoTab.IsSelected)
             {
-                
                 MostrarContacto();
             }
         }
 
         private void MostrarPersonas()
         {
-          
             List<string> personas = new List<string>
             {
-                  "Dianita ",
-               "Roimer",
-                 "Jimmy",
-               "Dayana ",
-                   "Andre"
-             };
+                "Dianita ",
+                "Roimer",
+                "Jimmy",
+                "Dayana ",
+                "Andre"
+            };
 
-          
-            InicioTab.Content = new System.Windows.Controls.ListBox { ItemsSource = personas };
+            InicioTab.Content = new ListBox { ItemsSource = personas, Background = System.Windows.Media.Brushes.LightGray, Foreground = System.Windows.Media.Brushes.Black, FontSize = 14, };
         }
 
         private void CargarProductos()
         {
             List<Producto> productos = new List<Producto>
- {
-        new Producto { NombreP = "Hydra-Lip Brillo Labial", Precio = 39.50 },
-        new Producto { NombreP = "Total Block Jumbo SPF 100", Precio = 65 },
-        new Producto { NombreP = "Ccori Crsital Rose", Precio = 95.50 },
-        new Producto { NombreP = "Desmaquillador Doble Fase", Precio = 37.50 },
-        new Producto { NombreP = "Mascarilla Antiarrugas y Antiedad", Precio = 14 },
-        new Producto { NombreP = "Totallist Aclarante Concha de nacar", Precio = 28.50 },
-        new Producto { NombreP = "Mascarilla Antiarrugas y Antiedad", Precio = 14 },
-        new Producto { NombreP = "Totallist Aclarante Concha de nacar", Precio = 28.50 },
-   
- };
+            {
+                new Producto { NombreP = "Hydra-Lip Brillo Labial", Precio = 39.50 },
+                new Producto { NombreP = "Total Block Jumbo SPF 100", Precio = 65 },
+                new Producto { NombreP = "Ccori Crsital Rose", Precio = 95.50 },
+                new Producto { NombreP = "Desmaquillador Doble Fase", Precio = 37.50 },
+                new Producto { NombreP = "Mascarilla Antiarrugas y Antiedad", Precio = 14 },
+                new Producto { NombreP = "Totallist Aclarante Concha de nacar", Precio = 28.50 },
+                new Producto { NombreP = "Mascarilla Antiarrugas y Antiedad", Precio = 14 },
+                new Producto { NombreP = "Totallist Aclarante Concha de nacar", Precio = 28.50 },
+             };
 
-            
-            ProductosTab.Content = new System.Windows.Controls.DataGrid { ItemsSource = productos };
+            DataGrid dataGridProductos = new DataGrid
+            {
+                ItemsSource = productos,
+                AutoGenerateColumns = true,
+                IsReadOnly = true,
+                Background = System.Windows.Media.Brushes.LightGray,
+                Foreground = System.Windows.Media.Brushes.Black
+            };
+            ProductosTab.Content = dataGridProductos;
+
         }
-
 
         private void MostrarContacto()
         {
-            
             List<Contacto> contactito = new List<Contacto>
             {
-                 new Contacto {NombreC = "JUAN ", Numero = "+51 945037467"  },
-               
+                new Contacto {NombreC = "JUAN ", Numero = "+51 945037467" }
              };
 
-            ContactoTab.Content = new System.Windows.Controls.DataGrid { ItemsSource = contactito};
+
+            DataGrid dataGridContacto = new DataGrid
+            {
+                ItemsSource = contactito,
+                AutoGenerateColumns = true,
+                IsReadOnly = true,
+                Background = System.Windows.Media.Brushes.LightGray,
+                Foreground = System.Windows.Media.Brushes.Black
+
+            };
+
+
+            ContactoTab.Content = dataGridContacto;
         }
-    
-       
 
         public class Producto
         {
             public string NombreP { get; set; }
             public double Precio { get; set; }
         }
+        public class Contacto
+        {
+            public string NombreC { get; set; }
+            public String Numero { get; set; }
+        }
         private void MainWindow_KeyDown(object sender, KeyEventArgs e)
         {
-          
             if (e.Key == Key.Escape)
             {
                 Menu m = new Menu();
                 m.Show();
                 this.Close();
             }
-
         }
-
-        public class Contacto
-        {
-            public string NombreC { get; set; }
-            public   String Numero { get; set; }
-        }
-
     }
 }
--- menuControl.xaml.cs
+++ menuControl.xaml.cs
@@ -2,14 +2,58 @@
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
+using System.Linq;
 
 namespace ControlNavegación
 {
     public partial class menuControl : Window
     {
+        private List<string> carrito = new List<string>();
+        private Dictionary<string, List<string>> productosCategoria;
+
+        public class ProductoItem
+        {
+            public string Name { get; set; }
+        }
+
         public menuControl()
         {
             InitializeComponent();
+            InitializeProductos();
+            InitializeCategoriaComboBox();
+        }
+
+        private void InitializeProductos()
+        {
+            productosCategoria = new Dictionary<string, List<string>>
+            {
+                {"Frutas", new List<string> {
+                    "Manzana", "Plátano", "Naranja", "Uva", "Fresa",
+                    "Pera", "Piña", "Mango", "Kiwi", "Sandía"
+                }},
+                {"Carnes", new List<string> {
+                    "Pollo", "Res", "Cerdo", "Pavo", "Cordero",
+                    "Pescado", "Atún", "Salmón", "Camarón", "Langosta"
+                }},
+                {"Lacteos", new List<string> {
+                    "Leche", "Queso", "Yogur", "Mantequilla", "Crema",
+                    "Helado", "Queso Crema", "Requesón", "Leche Condensada", "Crema Batida"
+                }},
+                {"Bebidas", new List<string> {
+                    "Coca-Cola", "Pepsi", "Sprite", "Fanta", "Agua",
+                    "Jugo de Naranja", "Cerveza", "Vino", "Té", "Café"
+                }}
+            };
+        }
+
+        private void InitializeCategoriaComboBox()
+        {
+            categoriaComboBox.Items.Add("Seleccione Categoría");
+            foreach (var categoria in productosCategoria.Keys)
+            {
+                categoriaComboBox.Items.Add(categoria);
+            }
+            categoriaComboBox.SelectedIndex = 0;
         }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
@@ -17,100 +61,147 @@ namespace ControlNavegación
             MenuItem menuItem = (MenuItem)sender;
             string header = menuItem.Header?.ToString();
 
-            if (header != null)
+            switch (header)
             {
-                switch (header)
+                case "Inicio":
+                    MostrarInicio();
+                    compradorPanel.Visibility = Visibility.Collapsed;
+                    break;
+                case "Continuar":
+                    compradorPanel.Visibility = Visibility.Visible;
+                    break;
+                case "Nuevo":
+                    NuevoCarrito();
+                    compradorPanel.Visibility = Visibility.Visible;
+                    break;
+                case "Salir":
+                    SalirAplicacion();
+                    break;
+                case "Frutas":
+                case "Carnes":
+                case "Lacteos":
+                case "Bebidas":
+                    MostrarProductos(header);
+                    break;
+            }
+        }
+
+        private void CategoriaComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            productoComboBox.Items.Clear();
+            string categoriaSeleccionada = categoriaComboBox.SelectedItem.ToString();
+
+            if (categoriaSeleccionada != "Seleccione Categoría" && productosCategoria.ContainsKey(categoriaSeleccionada))
+            {
+                productoComboBox.Items.Add("Seleccione Producto");
+                foreach (var producto in productosCategoria[categoriaSeleccionada])
                 {
-                    case "Inicio":
-                        MostrarInicio();
-                        break;
-                    case "Productos":
-                        
-                        break;
-                    case "Carrito de Compras":
-                        MostrarCarrito();
-                        break;
-                    case "Salir":
-                        SalirAplicacion();
-                        break;
-                    default:
-                        
-                        MostrarProductos(header);
-                        break;
+                    productoComboBox.Items.Add(producto);
+                }
+                productoComboBox.SelectedIndex = 0;
+            }
+        }
+
+        private void ProductoComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (productoComboBox.SelectedIndex > 0)
+            {
+                string productoSeleccionado = productoComboBox.SelectedItem.ToString();
+                if (!carrito.Contains(productoSeleccionado))
+                {
+                    carrito.Add(productoSeleccionado);
+                    ActualizarCarritoUI();
+                    productoComboBox.SelectedIndex = 0;
+                }
+                else
+                {
+                    MessageBox.Show("Este producto ya está en el carrito", "Aviso",
+                        MessageBoxButton.OK, MessageBoxImage.Information);
+                    productoComboBox.SelectedIndex = 0;
                 }
             }
+        }
+
+        private void RemoveFromCart_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            string producto = btn.Tag.ToString();
+            carrito.Remove(producto);
+            ActualizarCarritoUI();
         }
 
         private void MostrarInicio()
         {
             LimpiarContentGrid();
-            contentGrid.Children.Add(new TextBlock() { Text = "Estás en la página de Inicio." });
+            TextBlock textBlock = new TextBlock
+            {
+                Text = "Bienvenido a la tienda. Seleccione una categoría de productos.",
+                Foreground = System.Windows.Media.Brushes.White,
+                FontSize = 20,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center
+            };
+            contentGrid.Children.Add(textBlock);
         }
 
         private void MostrarProductos(string categoria)
         {
-            LimpiarContentGrid();
-            List<string> productos = ObtenerProductosPorCategoria(categoria);
-            ListBox listBoxProductos = new ListBox();
-            foreach (string producto in productos)
-            {
-                listBoxProductos.Items.Add(producto);
-            }
-            contentGrid.Children.Add(listBoxProductos);
+            var productos = productosCategoria[categoria]
+                .Select(p => new ProductoItem { Name = p })
+                .ToList();
+            productosItemsControl.ItemsSource = productos;
         }
 
-        private List<string> ObtenerProductosPorCategoria(string categoria)
+        private void ActualizarCarritoUI()
         {
-            switch (categoria)
-            {
-
-
-                case "Frutas":
-                    return new List<string> { "Manzana", "Plátano", "Naranja", "Uva", "Fresa", "Pera", "Piña", "Mango", "Kiwi", "Cereza", 
-                                              "Melón", "Sandía", "Limón", "Mandarina", "Papaya", "Granada", "Ciruela", "Higo", "Dátil", "Coco", 
-                                              "Guayaba", "Maracuyá", "Tamarindo", "Lichi", "Pomelo", "Mora", "Fruta de la pasión", "Carambola", 
-                                              "Kumquat", "Níspero", "Chirimoya", "Caqui", "Membrillo", "Zarzamora", "Pitahaya", "Nectarina", "Castaña",
-                                              "Cereza de Barbados", "Naranja sanguina", "Melocotón", "Níspero japonés", "Tamarillo", "Feijoa", "Rambután", 
-                                              "Tuna", "Grosella", "Ciruela pasa", "Yuzu", "Jabuticaba", "Noni" };
-                case "Carnes":
-
-                    return new List<string> { "Pollo", "Res", "Cerdo", "Pavo", "Cordero", "Pato", "Ternera", "Codorniz", "Conejo", "Liebre",
-                                              "Faisán", "Ciervo", "Búfalo", "Jabalí", "Carnero", "Cabra", "Avestruz", "Pichón", "Paloma", "Ganso", "Salmón", "Atún",
-                                              "Trucha", "Bacalao", "Lenguado", "Merluza", "Sardina", "Anchoa", "Bacalao", "Anguila", "Pejerrey", "Dorada", "Mero",
-                                              "Besugo", "Pez espada", "Tilapia", "Carpa", "Róbalo", "Calamar", "Pulpo", "Langosta", "Cangrejo", "Almeja", "Mejillón",
-                                              "Ostra", "Caracol", "Chorizo", "Salchicha", "Jamón" };
-                case "Lacteos":
-
-                    return new List<string> { "Leche", "Queso", "Yogur", "Mantequilla", "Nata", "Helado", "Postres lácteos", "Leche condensada", 
-                                              "Leche evaporada", "Leche en polvo", "Kéfir", "Crema agria", "Requesón", "Cuajada", "Queso crema", 
-                                              "Ricotta", "Mozzarella", "Gruyère", "Roquefort", "Brie", "Camembert", "Feta", "Provolone", "Gouda", "Emmental", "Manchego", 
-                                              "Parmesano", "Cheddar", "Gorgonzola", "Cottage cheese", "Mascarpone", "Crema de queso azul", 
-                                              "Leche de cabra", "Leche de oveja", "Leche de búfala", "Yogur griego", "Yogur de frutas", "Yogur natural", 
-                                              "Yogur desnatado", "Yogur sin lactosa", "Helado de vainilla", "Helado de chocolate", "Helado de fresa", 
-                                              "Helado de vainilla con trozos de galleta", "Helado de pistacho", "Helado de nueces", "Flan de vainilla" };
-                case "Bebidas":
-
-                    return new List<string> { "Coca-Cola", "Pepsi", "Sprite", "Fanta", "7Up", "Mirinda", "Dr Pepper", "Mountain Dew", "Schweppes", 
-                                              "Canada Dry", "Cerveza Corona", "Heineken", "Budweiser", "Guinness", "Stella Artois", "Modelo", "Victoria", 
-                                              "Tecate", "Dos Equis", "Red Bull", "Monster Energy", "Rockstar Energy", "Gatorade", "Powerade", "Arizona", 
-                                              "Snapple", "Nestea", "Lipton Ice Tea", "Tropicana", "Minute Maid", "Del Valle", "Fruitopia", "Ciel", 
-                                              "Bonafont", "Dasani", "San Pellegrino", "Perrier", "Evian", "Aquafina", "Volvic", "Topo Chico", "Jarritos", 
-                                              "Fresca", "Squirt", "A&W Root Beer", "Barq's", "Sunkist", "Canada Dry Ginger Ale" };
-
-                default:
-                    return new List<string>(); // Retorna una lista vacía si no se encuentra la categoría
-            }
+            carritoListBox.ItemsSource = null;
+            carritoListBox.ItemsSource = carrito;
         }
 
-        private void MostrarCarrito()
+        private void NuevoCarrito()
         {
-            LimpiarContentGrid();
-            contentGrid.Children.Add(new TextBlock() { Text = "Tu carrito de compras está vacío." });
+            carrito.Clear();
+            ActualizarCarritoUI();
+            nombreTextBox.Clear();
+            direccionTextBox.Clear();
+            telefonoTextBox.Clear();
+            categoriaComboBox.SelectedIndex = 0;
+        }
+
+        private void RealizarCompra_Click(object sender, RoutedEventArgs e)
+        {
+            if (carrito.Count == 0)
+            {
+                MessageBox.Show("El carrito está vacío", "Error",
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(nombreTextBox.Text) ||
+                string.IsNullOrWhiteSpace(direccionTextBox.Text) ||
+                string.IsNullOrWhiteSpace(telefonoTextBox.Text))
+            {
+                MessageBox.Show("Por favor complete todos los datos del comprador", "Error",
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            string productos = String.Join(", ", carrito);
+            MessageBox.Show(
+                $"Compra realizada por: {nombreTextBox.Text}\n" +
+                $"Dirección: {direccionTextBox.Text}\n" +
+                $"Teléfono: {telefonoTextBox.Text}\n\n" +
+                $"Productos:\n{productos}",
+                "Compra Exitosa", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            NuevoCarrito();
+            compradorPanel.Visibility = Visibility.Collapsed;
         }
 
         private void SalirAplicacion()
         {
-            MessageBoxResult result = MessageBox.Show("¿Estás seguro de que quieres salir?", "Salir", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            MessageBoxResult result = MessageBox.Show("¿Estás seguro de que quieres salir?", "Salir",
+                MessageBoxButton.YesNo, MessageBoxImage.Question);
             if (result == MessageBoxResult.Yes)
             {
                 Application.Current.Shutdown();
